0cf02e33b5da4ae5198107318bb90dac7790dfb1
grarrixf <164308236+grarrixf@users.noreply.github.com>
2024-04-27 16:13:23 +0800

    Update RecommenderSystem.py

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -79,8 +79,8 @@ if st.button('Get Recommendations'):
                         recommended_books = pd.concat([recommended_books, genre_recommended_books])
         st.write("## Recommended Books")
         for index, row in recommended_books.iterrows():
-            add_button = st.button(f'Add to Cart: {row["title"]}', key=f"add_{index}")  # Use row index as key
-            if add_button:
+            add_to_cart = st.checkbox(f'Add to Cart: {row["title"]}', key=f"add_{index}")
+            if add_to_cart:
                 st.session_state.cart.append(row['title'])
                 st.session_state.sync()  # Ensure session state is synchronized
             st.write(f"**Title:** {row['title']}")
