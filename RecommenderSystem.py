b32e4ab802264f15adda51f06d422c563c1ebcbd
grarrixf <164308236+grarrixf@users.noreply.github.com>
2024-04-28 13:53:52 +0800

    Update RecommenderSystem.py

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -89,7 +89,7 @@ if st.button('Get Recommendations'):
                         # Limit the number of recommended books for this genre
                         genre_recommended_books = genre_recommended_books.head(num_recommended_books)
                         # Add percentage column
-                        genre_recommended_books['percentage'] = genre_recommended_books['no'] / total_quantity
+                        genre_recommended_books['percentage'] = genre_recommended_books['no'] / total_quantity  # Calculate percentage
                         recommended_books = pd.concat([recommended_books, genre_recommended_books])
         
         # Sort recommended books by percentage
